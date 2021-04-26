2675f0b44d08d257d4a03368d439009327b1f661
NewballLee <NewballLee@boyaa.com>
2021-04-26 23:36:01 +0800

    feat:add redis demo

--- main.go
+++ main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/garyburd/redigo/redis"
 )
 
-func Find(slice []string, val string) bool {
-	if len(slice) <= 0 {
-		return false
-	}
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
+	var (
+		key      = "abc"
+		value int64 = 22
+		vKey      = "cbd"
+	)
+	r, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		fmt.Println("Connect to redis error", err)
+		return
+	}
+	defer r.Close()
 
-	fmt.Println(time.Now().Unix())
+	req, err := r.Do("set", key, value)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(req)
+	resp, err := redis.String(r.Do("get", vKey))
+	if err != nil && err != redis.ErrNil {
+		fmt.Println(err)
+	}
+	if err == redis.ErrNil {
+		fmt.Println("No data")
+	}
+
+	fmt.Println(resp)
+
 }
